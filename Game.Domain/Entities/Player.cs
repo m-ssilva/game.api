c9e9835bbea3cdf2d4b98c513ac7d1d75a62c960
m-ssilva <bleed-it-out@live.com>
2018-10-15 19:30:06 -0300

    feat(Player): add constructor and validators

--- Game.Domain/Entities/Player.cs
+++ Game.Domain/Entities/Player.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using Game.Domain.Enum;
 using Game.Domain.ValueObjects;
+using prmToolkit.NotificationPattern;
 
 namespace Game.Domain.Entities
 {
-    public class Player
+    public class Player : Notifiable
     {
+        public Player()
+        {
+
+        }
+
+        public Player(Mail mail, string password)
+        {
+            Mail = mail;
+            Password = password;
+
+            new AddNotifications<Player>(this)
+                .IfNotEmail(x => x.Mail.MailAddress, "Informe um e-mail válido")
+                .IfNullOrInvalidLength(x=> x.Password, 6, 32, "A senha deve ter entre 6 a 32 dígitos.");
+        }
+
         public Guid Id { get; set; }
         public Name Name { get; set; }
         public Mail Mail { get; set; }
