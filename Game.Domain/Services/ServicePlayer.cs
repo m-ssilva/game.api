7f10fb89df1e41ad0cd26dac3dbdb588d9da52ef
bleed-it-out@live.com <bleed-it-out@live.com>
2018-10-15 15:52:55 -0300

    refactor(Domain/Services): refactor playerService

--- Game.Domain/Services/ServicePlayer.cs
+++ Game.Domain/Services/ServicePlayer.cs
@@ -1,13 +1,24 @@
-﻿using Game.Domain.Arguments.Player;
+﻿using System;
+using Game.Domain.Arguments.Player;
+using Game.Domain.Interfaces.Repositories;
 using Game.Domain.Interfaces.Services;
 
 namespace Game.Domain.Services
 {
     public class ServicePlayer : IServicePlayer
     {
+        private readonly IRepositoryPlayer _repositoryPlayer;
+
+        public ServicePlayer(IRepositoryPlayer repositoryPlayer)
+        {
+            _repositoryPlayer = repositoryPlayer;
+        }
+
         public AddPlayerResponse AddPlayer(AddPlayerRequest request)
         {
-            throw new System.NotImplementedException();
+            Guid id = _repositoryPlayer.AddPlayer(request);
+            return new AddPlayerResponse() { Id = id, Message = "Operação realizada com sucesso!" };
+
         }
 
         public AuthenticatePlayerResponse Authenticate(AuthenticatePlayerRequest request)
