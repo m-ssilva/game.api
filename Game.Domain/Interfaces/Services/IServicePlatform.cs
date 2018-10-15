c7be7664c0a1a3254cc7eae52ab3ab34ab6da1b3
bleed-it-out@live.com <bleed-it-out@live.com>
2018-10-15 14:35:56 -0300

    refactor(Domain/Interfaces): changed interface service name

---
+++ Game.Domain/Interfaces/Services/IServicePlatform.cs
@@ -0,0 +1,9 @@
+﻿using Game.Domain.Arguments.Platform;
+
+namespace Game.Domain.Interfaces.Services
+{
+    public interface IServicePlatform
+    {
+        AddPlatformResponse AddPlatform(AddPlatformRequest request);
+    }
+}
