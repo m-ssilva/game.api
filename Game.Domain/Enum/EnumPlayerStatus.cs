45a42c738a701bf86cee47929ea0a9b337215b0a
bleed-it-out@live.com <bleed-it-out@live.com>
2018-10-15 14:27:43 -0300

    feat(enum): add enum folder

---
+++ Game.Domain/Enum/EnumPlayerStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Domain.Enum
+{
+    public enum EnumPlayerStatus
+    {
+        Pendente = 0,
+        Ativo = 1,
+        Bloqueado = 2
+    }
+}
