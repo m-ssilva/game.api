48e7155ded520b7b65d9455f09b15f9f2cdbb0c1
bleed-it-out@live.com <bleed-it-out@live.com>
2018-10-15 14:31:14 -0300

    refactor(Entities): updating player entity

--- Game.Domain/Entities/Player.cs
+++ Game.Domain/Entities/Player.cs
@@ -1,14 +1,15 @@
 ﻿using System;
+using Game.Domain.Enum;
+using Game.Domain.ValueObjects;
 
 namespace Game.Domain.Entities
 {
     public class Player
     {
         public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Mail { get; set; }
+        public Name Name { get; set; }
+        public Mail Mail { get; set; }
         public string Password { get; set; }
-        public int Status { get; set; }
+        public EnumPlayerStatus Status { get; set; }
     }
 }
