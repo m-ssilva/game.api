bebdae98fc1297ec5a1972781ab833e02127b65f
bleed-it-out@live.com <bleed-it-out@live.com>
2018-10-15 15:53:23 -0300

    feat(Repositories): created repositories folder

---
+++ Game.Domain/Interfaces/Repositories/IRepositoryPlayer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Game.Domain.Arguments.Player;
+
+namespace Game.Domain.Interfaces.Repositories
+{
+    public interface IRepositoryPlayer
+    {
+        AuthenticatePlayerResponse Authenticate(AuthenticatePlayerRequest request);
+        Guid AddPlayer(AddPlayerRequest request);
+    }
+}
