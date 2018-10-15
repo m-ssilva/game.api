4b50f251a5d5b48254abcb726a1c268f1077aba0
m-ssilva <bleed-it-out@live.com>
2018-10-15 19:31:41 -0300

    feat(Console): add console

---
+++ Game.AppConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using Game.Domain.Arguments.Player;
+using Game.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.AppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new ServicePlayer();
+
+            AuthenticatePlayerRequest request = new AuthenticatePlayerRequest();
+            service.Authenticate(request);
+        }
+    }
+}
