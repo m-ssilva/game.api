9e5a102fa866f41139662c53642c100e214bcf4e
bleed-it-out@live.com <bleed-it-out@live.com>
2018-10-15 14:30:02 -0300

    feat(services): add services folder

---
+++ Game.Domain/Services/ServicePlayer.cs
@@ -0,0 +1,18 @@
+﻿using Game.Domain.Arguments.Player;
+using Game.Domain.Interfaces.Services;
+
+namespace Game.Domain.Services
+{
+    public class ServicePlayer : IServicePlayer
+    {
+        public AddPlayerResponse AddPlayer(AddPlayerRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public AuthenticatePlayerResponse Authenticate(AuthenticatePlayerRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
